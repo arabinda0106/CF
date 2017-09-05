76163501904ed62a1d91dd00b94b85648de36973
arabinda0106 <arabinda0106@gmail.com>
2017-09-04 21:51:47 -0700

    Added files for Sep 4th

--- CodeFights/GoBot.cs
+++ CodeFights/GoBot.cs
@@ -8,7 +8,7 @@ namespace CodeFights
 {
     public class GoBot
     {
-        /*   
+        /*
         GoDaddy makes a lot of different top-level domains available to its customers. A top-level domain is one that goes directly after the last dot ('.') in the domain name, for example .com in example.com. To help the users choose from available domains, GoDaddy is introducing a new feature that shows the type of the chosen top-level domain. You have to implement this feature.
         To begin with, you want to write a function that labels the domains as "commercial", "organization", "network" or "information" for .com, .org, .net or .info respectively.
         For the given list of domains return the list of their labels.
---
+++ CodeFightsUnitTestProject/CF_Interview_Practice_Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeFights;
+
+namespace CodeFightsUnitTestProject
+{
+    [TestClass]
+    public class CF_Interview_Practice_Test
+    {
+        [TestMethod]
+        public void tripletSumTest()
+        {
+            //arrange
+
+            int x = 0;
+            int[] a = new int[] { };
+
+            //act
+            CF_Interview_Practice cfipt = new CF_Interview_Practice();
+
+            bool result = cfipt.tripletSum(x, a);
+
+            //assert
+        }
+    }
+}
